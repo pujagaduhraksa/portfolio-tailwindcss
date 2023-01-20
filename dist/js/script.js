c076fe110beef5a71a88e97cf22d059cb024dab5
Puja Raksa <pujaraksa.id@gmail.com>
2023-01-20 14:34:55 +0800

    Fix Hamburger Menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,10 +11,23 @@ hamburger.addEventListener('click', function(){
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
+
+// Outside Hamburger
+window.addEventListener('click', function(e){
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
